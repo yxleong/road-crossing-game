0e1a5c0acc8d5c90c96b6a2b9b96651185c065ed
rubeuzx <104368308+rubeuzx@users.noreply.github.com>
2023-12-11 15:29:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+"""
+Program : main.py
+Author : Group 10
+Analysis:
+1. This file serves as the entry point of the program.
+2. It imports the Menu class from main_menu.py and initializes the main application.
+
+Design - pseudocode:
+1. Define the significant constant
+   None in this file.
+2. The inputs are
+   None in this file.
+3. Computations:
+   Import Menu class from main_menu module.
+   Create an instance of the Menu class.
+   Start the main event loop of the Tkinter application.
+4. The output is
+   None in this file.
+"""
+
 # Importing the Menu class from the main_menu module
 from main_menu import Menu
 
