a3e91d86ed1a34b9bf31dc00ee631751c8bdd301
yxleong <yx0829leong@gmail.com>
2023-12-14 00:32:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 """
 Program : main.py
-Author : Group 10
+Author : GROUP 10
+            林佩佩 B10915067
+            羅翡瑩 B11015010
+            盧清珍 B11015012
+            梁婭瑄 B11015016
 Analysis:
 1. This file serves as the entry point of the program.
 2. It imports the Menu class from main_menu.py and initializes the main application.
 
 Design - pseudocode:
-1. Define the significant constant
-   None in this file.
-2. The inputs are
-   None in this file.
-3. Computations:
-   Import Menu class from main_menu module.
-   Create an instance of the Menu class.
-   Start the main event loop of the Tkinter application.
-4. The output is
-   None in this file.
+1. Import required module
+      main_menu
+2. Create an instance of the Menu class
+      main_app
+3. Start the main event loop
 """
 
 # Importing the Menu class from the main_menu module
