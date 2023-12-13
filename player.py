ed9392758516d82a36cef177f1c19c63fcb47585
yxleong <yx0829leong@gmail.com>
2023-12-14 01:46:47 +0800

    Update player.py

--- player.py
+++ player.py
@@ -9,23 +9,51 @@ Analysis:
 1. Represents the player character in the game, handles movement and collisions.
 
 Design - pseudocode:
-1. Import required module
-      Turtle
+1. Import required modules
+    Turtle
+    register_shape
 2. Define the significant constant
-   STARTING_POSITION, MOVE_DISTANCE, FINISH_LINE_Y: Player starting position, movement distance, and finish line position.
-   POSITION_LIMIT_X, POSITION_LIMIT_Y: Player movement limits.
-   PLAYER_IMG: Path to the image representing the player.
+    STARTING_POSITION, MOVE_DISTANCE, FINISH_LINE_Y: Player starting position, movement distance, and finish line position.
+    POSITION_LIMIT_X, POSITION_LIMIT_Y: Player movement limits.
+    PLAYER_IMG: Path to the image representing the player.
 3. Define the Player class
-   Register the player image
-   Initialize the Player as a Turtle
-   initialize attributes for the player
-   Define a method to move the player forward
-   Define a method to move the player backward
-   Define a method to move the player to the left
-   Define a method to move the player to the right
-   Define a method to check if the player has reached the finish line
-   Define a method to check for collision with cars
-   Define a method to check for drowning in rivers
+    Initialize Player as a Turtle with the player image
+        Register the player image
+        Lift the pen to avoid drawing while moving
+        Set the player's color to lime
+        Set the player's orientation to face upward
+        Place the player at the starting position
+        Initialize the is_alive flag as True
+    Define a method to move the player forward
+        If the player is alive
+            Move the player forward by the move distance
+    Define a method to move the player backward, with boundary checking
+        If the player is alive and the player's y-coordinate is above the lower limit
+            Move the player backward by the move distance
+    Define a method to move the player to the left, with boundary checking
+        If the player is alive and the player's x-coordinate is above the left limit
+            Move the player to the left by the move distance
+    Define a method to move the player to the right, with boundary checking
+        If the player is alive and the player's x-coordinate is below the right limit
+            Move the player to the right by the move distance
+    Define a method to check if the player has reached the finish line
+        If the player's y-coordinate is above the finish line
+            Move the player to the starting position
+            Return True indicating that the player crossed the finish line
+        Return False indicating that the player has not crossed the finish line
+    Define a method to check for collision with cars
+        For each car in the list of cars
+            If the player's y-coordinate is within a range around the car's y-coordinate
+                and the distance between the player and the car is within a threshold
+                Set the player as not alive
+                Return True indicating a collision
+        Return False indicating no collision
+    Define a method to check for drowning in rivers
+        For each river segment in the list of rivers
+            If the player's position is within the boundaries of the river segment
+                Set the player as not alive
+                Return True indicating drowning
+        Return False indicating no drowning
 """
 
 from turtle import Turtle, register_shape
